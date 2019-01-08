0636dc7dcb4d5607194b7650da41ec626f3dddfa
Mathilde Scebalt <mathilde.scebalt@systeme-u.fr>
2019-01-08 17:52:16 +0100

    add folder src

---
+++ src/objects/business/be/UserBE.js
@@ -0,0 +1,8 @@
+export class UserBE {
+	constructor(firstname, lastname, email, lastConnection) {
+		this.firstname = firstname
+		this.lastname = lastname
+		this.email = email
+		this.lastConnection = lastConnection
+	}
+}
